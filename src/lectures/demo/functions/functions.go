c0769e310188821a93617abb045e6e5c8fdda3d8
Boris Petrov <boris.petrov88@abv.bg>
2026-06-29 15:42:55 +0300

    implemented demo functions

--- src/lectures/demo/functions/functions.go
+++ src/lectures/demo/functions/functions.go
@@ -1,6 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func double(x int) int {
+	return x + x
 }
 
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
+
+func greet() {
+	fmt.Println("Hello from greet function!")
+}
+func main() {
+	greet()
+
+	dozen := double(6)
+	fmt.Println("A dozen is ", dozen)
+
+	bakersDozen := add(dozen, 1)
+	fmt.Println("A bakers dozen is", bakersDozen)
+
+	anotherBakersDozen := add(double(6), 1)
+	fmt.Println("another's baker dozen is", anotherBakersDozen)
+}
